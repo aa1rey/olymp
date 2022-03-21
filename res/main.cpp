817f29b7edcc485b91e6c7185e8afd9e71ca733e
aa1rey <bigfinger.bf@gmail.com>
2022-03-21 21:33:02 +0300

    Добавлены переменные направления движения вправо и назад. А так же научил Ivan'a определять, где какое направление. После движения прямо пока не слева свободно переназначил переменные направления движения относительно того, куда он на этот раз пошел.

--- res/main.cpp
+++ res/main.cpp
@@ -9,38 +9,75 @@ using namespace std;
 Fairyland world; // инициализация мира
 
 int move() {
-	Direction leftDirection; // переменная левого направления
-	Direction frontDirection; // переменная прямого направления
-	for (int i = 0; i < 1000; i++) {
+
+	for (int i = 0; i < 1; i++) {
+
+		Direction leftDirection; // переменная левого направления
 
 		// Назначаем левое направление, исходя из того, куда можем пойти
 
 		if (world.canGo(Character::Ivan, Direction::Down)) {
 			leftDirection = Direction::Right;
 		}
-		if (world.canGo(Character::Ivan, Direction::Left)) {
+		else if (world.canGo(Character::Ivan, Direction::Left)) {
 			leftDirection = Direction::Down;
 		}
-		if (world.canGo(Character::Ivan, Direction::Up)) {
+		else if (world.canGo(Character::Ivan, Direction::Up)) {
 			leftDirection = Direction::Left;
 		}
-		if (world.canGo(Character::Ivan, Direction::Right)) {
+		else if (world.canGo(Character::Ivan, Direction::Right)) {
 			leftDirection = Direction::Up;
 		}
-		
+
+		Direction frontDirection; // переменная прямого направления
+
 		// Назначаем прямое направление, исходя из того, куда можем пойти
 
 		if (world.canGo(Character::Ivan, Direction::Down)) {
 			frontDirection = Direction::Down;
 		}
-		if (world.canGo(Character::Ivan, Direction::Left)) {
+		else if (world.canGo(Character::Ivan, Direction::Left)) {
 			frontDirection = Direction::Left;
 		}
-		if (world.canGo(Character::Ivan, Direction::Up)) {
+		else if (world.canGo(Character::Ivan, Direction::Up)) {
 			frontDirection = Direction::Up;
 		}
-		if (world.canGo(Character::Ivan, Direction::Right)) {
+		else if (world.canGo(Character::Ivan, Direction::Right)) {
 			frontDirection = Direction::Right;
+		}
+
+		Direction rightDirection; // переменная правого направления
+
+		// Назначаем правое направление, исходя из того, куда можем пойти
+
+		if (world.canGo(Character::Ivan, Direction::Down)) {
+			rightDirection = Direction::Left;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Left)) {
+			rightDirection = Direction::Up;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Up)) {
+			rightDirection = Direction::Right;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Right)) {
+			rightDirection = Direction::Down;
+		}
+
+		Direction backDirection; // переменная заднего направления
+
+		// Назначаем заднее направление, исходя из того, куда можем пойти
+
+		if (world.canGo(Character::Ivan, Direction::Down)) {
+			backDirection = Direction::Up;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Left)) {
+			backDirection = Direction::Right;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Up)) {
+			backDirection = Direction::Down;
+		}
+		else if (world.canGo(Character::Ivan, Direction::Right)) {
+			backDirection = Direction::Left;
 		}
 
 		// пока не можем пойти налево, идем прямо
@@ -48,6 +85,27 @@ int move() {
 		while (!world.canGo(Character::Ivan, leftDirection)) {
 			world.go(frontDirection, Direction::Pass);
 		}
+
+		if (world.canGo(Character::Ivan, leftDirection)) {
+			world.go(leftDirection, Direction::Pass);
+		}
+
+		Direction reservDirection = rightDirection;
+		rightDirection = frontDirection;
+		frontDirection = leftDirection;
+		leftDirection = backDirection;
+		backDirection = reservDirection;
+
+		// хочу запретить ему рассматривать поворот направо и назад. но у меня не получается понять как это сделать. Ниже по логике написал, как думал можно. Но Visual Studio выебывается.
+		// функцию canGo() типа bool нельзя изменить, потому что как я понял, она является константой. Но как тогда запретить ему проверять, может ли он пойти назад или вправо? 
+
+		if (world.canGo(Character::Ivan, rightDirection)) {
+			world.canGo(Character::Ivan, rightDirection) = false;
+
+		}
+
+			
+
 	}
 	return 0;
 }
